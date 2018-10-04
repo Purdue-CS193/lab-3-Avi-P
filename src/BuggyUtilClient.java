d76967532933ff2977710a2788583072eab2dfdb
Avi-P <apooruli@gmail.com>
2018-10-04 13:58:52 -0400

    Some random changes

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -3,10 +3,10 @@ public class BuggyUtilClient {
     public static void main(String[] args) {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
-        System.out.println("In: first = " + "\"Sean\"" +
-                ", last = " + "\"Flannery\"");
+        System.out.println("In: first = " + "\"Avinash\"" +
+                ", last = " + "\"Pooruli\"");
         System.out.println("Out: " +
-        StringUtils.makePurdueUsername("Sean", "Flannery"));
+        StringUtils.makePurdueUsername("Avinash", "Pooruli"));
     }
 }
 
